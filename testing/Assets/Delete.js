8e055411af82a59bfbc65d5ca27cf15eb1a66247
April Joyce F. Magpantay <aprilmagpantay_12@yahoo.com>
2017-10-17 03:15:30 +0800

    Second Commit
    Keypad System with Sounds. To add: student database

---
+++ testing/Assets/Delete.js
@@ -0,0 +1,57 @@
+﻿#pragma strict
+
+import System.Collections;
+import UnityEngine;
+import UnityEngine.UI;
+var button : UI.Image;
+var code : UI.Text;
+
+var Hover : Color;
+var Original : Color;
+private var timer : float;
+private var gazeTime : float = 1.5f;
+private var gazedAt : boolean;
+
+public var beepaudio : AudioSource;
+
+function Start () {
+	//code = GetComponent(Text);
+	//audio = GetComponent.<AudioSource>();
+}
+
+
+function Update () {
+	if (gazedAt) {
+			timer += Time.deltaTime;
+
+				//if (timer >= gazeTime) {
+				//	SceneManager.LoadScene("Scene1");
+			//	}
+			//button.Color=Hover;
+			if (timer >= gazeTime) {
+						code.text = "";
+						KeypadSystem_.maxNumbers = 0;
+						beepaudio.volume = 0.3;
+						beepaudio.pitch = 0.8;
+						beepaudio.Play();
+						Debug.Log ("1 is clicked");
+						timer=0f;
+					}
+				}
+}
+
+public function PointerEnter(){
+		gazedAt=true;
+		Debug.Log ("Pointer Enter");
+		button.color = Hover;
+		beepaudio.volume = 0.3;
+		beepaudio.pitch = 1;
+		beepaudio.Play();
+}
+
+public function PointerExit(){
+		gazedAt=false;
+		Debug.Log ("Pointer Exit");
+		beepaudio.Stop();
+		button.color = Original;
+}
