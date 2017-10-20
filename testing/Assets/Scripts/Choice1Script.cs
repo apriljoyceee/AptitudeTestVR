72f3d4dcd87dc3b30996ff7e0e3cebe42d271334
April Joyce F. Magpantay <aprilmagpantay_12@yahoo.com>
2017-10-20 23:12:58 +0800

    Fifth Commit
    30% Thesis.

---
+++ testing/Assets/Scripts/Choice1Script.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System;
+using System.Data;
+using Mono.Data.Sqlite;
+
+public class Choice1Script : MonoBehaviour {
+
+	private float timer;
+	private float gazeTime =2.5f;
+	private bool gazedAt;
+	private string connectionString;
+
+	private List<HighScore> highScores = new List<HighScore>();
+
+	public GameObject scorePrefab;
+	public Transform scoreParent;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (gazedAt) {
+			timer += Time.deltaTime;
+
+			if (timer >= gazeTime) {
+				connectionString = "URI=file:" + Application.dataPath + "/testingdb.sqlite";
+				ShowScores();
+			}
+		}
+
+	}
+		
+	public void PointerEnter(){
+		//Debug.Log ("Pointer Enter");
+		gazedAt=true;
+	}
+	public void PointerExit(){
+		//Debug.Log ("Pointer Enter");
+		gazedAt=false;
+	}
+	private void GetScores()
+	{
+
+		using (IDbConnection dbConnection = new SqliteConnection(connectionString))
+		{
+			dbConnection.Open();
+
+			using (IDbCommand dbCmd = dbConnection.CreateCommand())
+			{
+				string sqlQuery = "SELECT * FROM HighScores";
+
+				dbCmd.CommandText = sqlQuery;
+
+				using (IDataReader reader = dbCmd.ExecuteReader())
+				{
+					while (reader.Read())
+					{
+						highScores.Add (new HighScore (reader.GetInt32 (0), reader.GetString (2), reader.GetString (1), reader.GetString (3)));
+					}
+					//dbConnection.Close();
+					//reader.Close();
+				}
+			}
+		}
+
+	}
+
+	private void ShowScores()
+	{
+		GetScores ();
+		Debug.Log ("PUMASOK KABA ULI GAGO?s");
+
+		GameObject tmpObjec = Instantiate (scorePrefab);
+
+		HighScore tmpScore = highScores[1];
+		tmpObjec.GetComponent<HighScoreScript>().SetScore(tmpScore.Name,tmpScore.Choice1,tmpScore.Choice2, "#" + tmpScore.ID.ToString());
+		tmpObjec.transform.SetParent (scoreParent);
+
+	}
+}
---
+++ testing/Assets/Scripts/HighScore.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+class HighScore
+{
+	public string Choice1 { get; set; }
+
+	public string Name { get; set; }
+
+	public string Choice2 { get; set; }
+
+	public int ID { get; set; }
+
+	public HighScore(int id, string choice1, string name, string choice2)
+	{
+		this.Choice1 = choice1;
+		this.Name = name;
+		this.ID = id;
+		this.Choice2 = choice2;
+	}
+}
