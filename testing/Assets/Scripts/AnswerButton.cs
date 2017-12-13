5ca2e63eb9f4b57e142b1d7c63dc6957c26e4cff
April Joyce F. Magpantay <aprilmagpantay_12@yahoo.com>
2017-12-13 13:08:06 +0800

    Ninth Commit
    Questions

---
+++ testing/Assets/Scripts/AnswerButton.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AnswerButton : MonoBehaviour {
+
+	private float timer;
+	private float gazeTime =2.5f;
+	private bool gazedAt;
+	public Text answerText;
+	private GameController gameController;
+	private AnswerData answerData;
+
+	// Use this for initialization
+	void Start () {
+		gameController = FindObjectOfType<GameController> ();
+	}
+	public void Setup(AnswerData data){
+		answerData = data;
+		answerText.text = answerData.answerText;
+	}
+	void Update () {
+		if (gazedAt) {
+			timer += Time.deltaTime;
+
+			if (timer >= gazeTime) {
+				gameController.AnswerButtonClicked (answerData.isCorrect);
+				gazedAt=false;
+			}
+		}
+
+	}
+	public void PointerEnter(){
+		//Debug.Log ("Pointer Enter");
+		gazedAt=true;
+	}
+	public void PointerExit(){
+		//Debug.Log ("Pointer Enter");
+		gazedAt=false;
+	}
+}
